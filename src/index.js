118381832d7dcaec44321cbff8a4ede44bf8e59f
OlenaPut <elena.vput@gmail.com>
2023-05-16 15:24:31 +0300

    clearing the search field after entering data

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ function toFahrenheit(event) {
 }
 
 let tempUnitF = document.querySelector("#tempUnitF");
+
 tempUnitF.addEventListener("click", toFahrenheit);
 
 //Define current data
@@ -89,6 +90,7 @@ function showCurrentTemp(response) {
   let feelsLike = Math.round(response.data.main.feels_like);
   let feelsLikeData = document.querySelector("#feelsLike");
   feelsLikeData.innerHTML = feelsLike;
+  document.querySelector("#citySearch").value = "";
 }
 
 let form = document.querySelector("form");
